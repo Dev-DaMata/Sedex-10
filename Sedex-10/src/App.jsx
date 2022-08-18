ffa57465bdd27d391fcdff88086632bbd6d165e3
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2022-08-18 20:18:05 -0300

    continuando a requisicao da via cep

--- Sedex-10/src/App.jsx
+++ Sedex-10/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './components/Header/Header'
 import Input from './components/Input/Input'
-
+import Requisicao from './components/Requisicao/Requisicao'
 function App() {
   return (
     <div className="App">
       <Header/>
       <Input/>
+      <Requisicao/>
     </div>
   )
 }
--- Sedex-10/src/components/Requisicao/Requisicao.jsx
+++ Sedex-10/src/components/Requisicao/Requisicao.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { useState } from 'react'
+
 const Requisicao = () => {
     const [informacoes, setInformacoes] = useState()
-
-    async function handleRequisicao(cep){
-        const response = await fetch(`https://viacep.com.br/ws/${cep}`)
+    const [cep, setCep] = useState()
+    const inputCep = (e)=>{
+        setCep(e.target.value)
+    }
+    async function handleCep(){
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json`)
         const json = await response.json()
         setInformacoes(json)
     }
+
   return (
     <div>
-        
-            
+        <input type="text" onChange={inputCep} />
+        <button onClick={handleCep}>buscar</button>
+        <h1>{informacoes ? informacoes.cep : "carregando"}</h1>
+        <img src={informacoes ? informacoes.logradouro : ""} alt="" />
     </div>
   )
 }
-
 export default Requisicao

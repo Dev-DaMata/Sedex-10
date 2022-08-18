77efef2918d19333cd97b673dcced9c41eb3d383
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2022-08-18 19:34:25 -0300

    Header iniciado

---
+++ Sedex-10/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import S from './Header.module.css'
+const Header = () => {
+  return (
+    <div>
+        <h1 className={S.nome}>Sedex-10</h1>
+    </div>
+  )
+}
+
+export default Header

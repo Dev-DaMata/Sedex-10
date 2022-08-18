f3b216b33d3609c572e79c587127b75d420125b2
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2022-08-18 20:33:34 -0300

    requisição funcionando

--- Sedex-10/src/App.jsx
+++ Sedex-10/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 import Header from './components/Header/Header'
-import Input from './components/Input/Input'
 import Requisicao from './components/Requisicao/Requisicao'
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Input/>
       <Requisicao/>
     </div>
   )
--- Sedex-10/src/components/Requisicao/Requisicao.jsx
+++ Sedex-10/src/components/Requisicao/Requisicao.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import S from './Requisicao.module.css'
 
 const Requisicao = () => {
     const [informacoes, setInformacoes] = useState()
@@ -8,17 +9,20 @@ const Requisicao = () => {
         setCep(e.target.value)
     }
     async function handleCep(){
-        const response = await fetch(`https://viacep.com.br/ws/${cep}/json`)
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         const json = await response.json()
         setInformacoes(json)
+        console.log(json);
     }
 
   return (
-    <div>
-        <input type="text" onChange={inputCep} />
-        <button onClick={handleCep}>buscar</button>
+    <div className={S.cep}>
+        <input type="text" value={cep} onChange={inputCep} />
+        <button onClick={handleCep}>buscar cep</button>
         <h1>{informacoes ? informacoes.cep : "carregando"}</h1>
-        <img src={informacoes ? informacoes.logradouro : ""} alt="" />
+        <p>{informacoes ? informacoes.localidade : "carregando"}</p>
+        <p>{informacoes ? informacoes.bairro : "carregando"}</p>
+        <p>{informacoes ? informacoes.logradouro : "carregando"}</p>
     </div>
   )
 }
